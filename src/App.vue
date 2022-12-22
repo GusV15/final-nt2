1c9bd0f9941b0e775e82fde4b8a8ae680223915e
gusV15 <gustavov0706@gmail.com>
2022-12-22 20:11:49 -0300

    Resolución final nt2

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <ConversorDeTemperatura></ConversorDeTemperatura>
+  </div>
+</template>
+
+<script>
+import ConversorDeTemperatura from "./components/ConversorDeTemperatura.vue";
+export default {
+  name: "App",
+  components: {
+    ConversorDeTemperatura,
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/ConversorDeTemperatura.vue
@@ -0,0 +1,63 @@
+<template lang="html">
+
+  <section class="container">
+    <h1 class="title">Conversor de Temperatura</h1>
+    <hr />
+    <div>
+      <p>Ingrese Temperatura °C</p>
+      <input type="number" @input="calcularTemperaturas($event)"/> 
+    </div>
+    <div>
+      <p>Con temperatura °F:</p> <span :style="setColorByTemp(tempEnCentigrados)">{{tempEnFarenheit}}</span>
+      <p>Con temperatura °K:</p> <span :style="setColorByTemp(tempEnCentigrados)">{{tempEnKelvin}}</span>
+    </div>
+    <hr />
+    <h2>Respuestas</h2>
+    <p>1:c 2:b 3:d 4:c 5:b</p>
+  </section>
+
+</template>
+
+<script lang="js">
+
+  export default  {
+    name: 'form-view',
+    props: [],
+    mounted () {
+
+    },
+    data () {
+      return {
+        tempEnCentigrados: '',
+        tempEnFarenheit: '',
+        tempEnKelvin: '',
+      }
+    },
+    methods: {
+      calcularTemperaturas({target}) {
+        this.tempEnCentigrados = target.value;
+        this.tempEnFarenheit = this.tempEnCentigrados ? ((+this.tempEnCentigrados*(1.8)) + 32) : '';
+        this.tempEnKelvin = this.tempEnCentigrados ? (+this.tempEnCentigrados + 273.15) : '';
+      },
+      setColorByTemp(temp) {
+        let color = 'red';
+        if (temp <= 0) {
+          color = 'blue';
+        } else if(temp > 0 && temp < 15) {
+          color = 'magenta';
+        }
+        return { color };
+      },
+    },
+    computed: {
+    }
+}
+
+
+</script>
+
+<style scoped lang="css">
+.title {
+  color: red;
+}
+</style>
